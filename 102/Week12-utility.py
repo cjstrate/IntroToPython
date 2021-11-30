d009bb90d5ecb95edcf386bb8b199381ac853d26
cjstrate <carterstrate26@gmail.com>
2021-11-29 21:35:56 -0700

    load_file() funtion completed
    I also added a test text file for testing purposes

---
+++ 102/Week12-utility.py
@@ -0,0 +1,9 @@
+# Carter Strate
+# CSCI 102 - Section D
+# Week 12 - Utilty using Git and Increment Development
+# References: None
+# Time: TBD
+
+def load_file(file):
+    with open(file,'r') as text:
+        return text.read().split('\n')
