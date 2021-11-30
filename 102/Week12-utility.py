edd7889e76fdc1fa23ed20822a1b3467bb04bf63
cjstrate <carterstrate26@gmail.com>
2021-11-29 21:52:10 -0700

    Finished find_word_count() funtion
    May need more work but I will see. I also made some changes to the test file.

--- 102/Week12-utility.py
+++ 102/Week12-utility.py
@@ -11,3 +11,10 @@ def load_file(file):
 def update_string(string1,string2,index):
     out = string1[:index]+string2+string1[index+1:]
     print(f'OUTPUT {out}')
+
+def find_word_count(file,word):
+    tcount = 0
+    text = load_file(file)
+    for line in text:
+        tcount += line.count(word)
+    return tcount
